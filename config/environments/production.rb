85ad892e33553403620c4665b3d48d6e3715efa4
Rajat Gupta <rgupta@qubole.com>
2013-07-08 14:03:59 +0200

    some fixes.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Trend::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  # Do not compress assets
+  config.assets.compress = false
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -15,10 +18,8 @@ Trend::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
-  # Generate digests for assets URLs
-  config.assets.digest = true
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
