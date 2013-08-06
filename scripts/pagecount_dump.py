08fe12742862f132e07e0832e27aaa6f7694efbd
Rajat Venkatesh <rvenkatesh@qubole.com>
2013-08-06 23:00:01 +0530

    Clean up options parsers. Add comments

--- scripts/pagecount_dump.py
+++ scripts/pagecount_dump.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python
+"""
+  A script to download pagecount datafeed from http://dumps.wikimedia.org/other/pagecounts-raw/.
+  
+  Accepts the following arguments:
+  1. Year (Defaults to current year)
+  2. Month (Defaults to current month)
+  3. Date (Defaults to current date)
+  4. Temporary location
+  5. Destination S3 Path
+
+  Make sure that the local machine has atleast 4-5GB of disk space free.
+"""
+
 from calendar import monthrange
 import requests
 import sys
@@ -27,7 +40,7 @@ def download_url(url, path):
   sys.stdout.write('\n')
 
 def upload_file_to_s3(path, s3_url):
-  subprocess.check_call(["s3cmd", "-c", "/usr/lib/hustler/creds/s3cfg", "sync", "%s/" % path, s3_url])
+  subprocess.check_call(["s3cmd", "sync", "%s/" % path, s3_url])
 
 def cleanup(path):
   subprocess.check_call(["rm", "-r", path])
@@ -49,23 +62,27 @@ def main(year, month, day, temp_loc, s3_loc):
   cleanup(temp_loc)
 
 parser = OptionParser()
-parser.add_option("-y", "--year", dest="year", help = "Download files for this year")
-parser.add_option("-m", "--month", dest="month", help = "Download files for this month")
-parser.add_option("-d", "--day",  dest="day", help = "Download files for this day")
-parser.add_option("-t", "--temp", dest="temp_loc", help = "Temp location in local filesytem")
+parser.add_option("-y", "--year", dest="year", type="int", help = "Download files for this year")
+parser.add_option("-m", "--month", dest="month", type="int", help = "Download files for this month")
+parser.add_option("-d", "--day",  dest="day", type="int", help = "Download files for this day")
+parser.add_option("-t", "--temp", dest="temp_loc", default="/tmp/", help = "Temp location in local filesytem")
 parser.add_option("-s", "--s3", dest="s3url", help = "S3 location")
 (options, args) = parser.parse_args()
+
+if not options.s3url:
+  parser.error("S3 Location has not been specified")
+
 now = date.today() - timedelta(days=1)
 year = now.year
 month = now.month
 day = now.day
 
 if not options.year is None:
-  year = int(options.year)
+  year = options.year
 if not options.month is None:
-  month = int(options.month)
+  month = options.month
 if not options.day is None:
-  day = int(options.day)
+  day = options.day
 
 
 main(year, month, day, options.temp_loc, options.s3url)
